61469ce3d9e3f027784ad47d9341fe148923db1f
Ansahmbom <ansahmbomyong@gmail.com>
2025-03-13 10:19:22 +0200

    Updated settings.py

--- JobBoard/settings.py
+++ JobBoard/settings.py
@@ -27,15 +27,15 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$djo^*3v3%xoex8)j@#%sv5*5yj#n$+=zh3d##bmq1l^f#evyq"
-# SECRET_KEY = env.str("SECRET_KEY")  # For Heroku
+# SECRET_KEY = "django-insecure-$djo^*3v3%xoex8)j@#%sv5*5yj#n$+=zh3d##bmq1l^f#evyq"
+SECRET_KEY = env.str("SECRET_KEY")  # For Heroku
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = env.bool("DEBUG", default=False) == "True"  # For Heroku
+# DEBUG = True
+DEBUG = env.bool("DEBUG", default=False) == "True"  # For Heroku
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ["jobboard-ansa.herokuapp.com", "localhost", "127.0.0.1"] # For Heroku
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["jobboard-ansa.herokuapp.com", "localhost", "127.0.0.1"]  # For Heroku
 
 
 # Application definition
@@ -143,10 +143,10 @@ DEFAULT_FROM_EMAIL = env(
 )
 
 # Celery Configuration
-CELERY_BROKER_URL = "redis://localhost:6379/0"
-# CELERY_BROKER_URL = os.environ.get(
-#     "REDIS_URL", "redis://localhost:6379/0"
-# )  # For Heroku
+# CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_BROKER_URL = os.environ.get(
+    "REDIS_URL", "redis://localhost:6379/0"
+)  # For Heroku
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 
@@ -188,18 +188,18 @@ CACHES = {
 #     }
 # }
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST"),
-        "PORT": env("DB_PORT"),  # Leave empty if using default port
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": env("DB_NAME"),
+#         "USER": env("DB_USER"),
+#         "PASSWORD": env("DB_PASSWORD"),
+#         "HOST": env("DB_HOST"),
+#         "PORT": env("DB_PORT"),  # Leave empty if using default port
+#     }
+# }
 
-# DATABASES = {"default": env.db("DATABASE_URL")}   # For Heroku
+DATABASES = {"default": env.db("DATABASE_URL")}  # For Heroku
 
 
 # Password validation
@@ -248,4 +248,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "user_management.User"
 
 # Deploying with Heroku
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
